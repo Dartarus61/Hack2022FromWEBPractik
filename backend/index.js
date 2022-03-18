99d1f76d1647427459c372d9e7e5ce890cd332e0
Dartarus61 <76129308+Dartarus61@users.noreply.github.com>
2022-03-18 19:44:52 +0300

    master

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get("/api", (req, res) => {
+  res.json({
+    message: "hello",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is started on ${PORT}`);
 });
